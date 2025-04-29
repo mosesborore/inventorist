ac0c73ef249eaf1c6cd61786d01b5702b052dec9
Moses Borore <mosesbororekinuthia@gmail.com>
2025-04-29 21:22:16 +0300

    Remove unused mutation func.

--- frontend/app/mutations.ts
+++ frontend/app/mutations.ts
@@ -30,31 +30,3 @@ export const useCreateSeller = () => {
     },
   });
 };
-
-export const useCreateInvoice = () => {
-  const url = buildURLWithBase("/invoices/seller");
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: async (formData: SellerSchemaType) => {
-      const res = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
-
-      if (!res.ok) {
-        const error = await res.json();
-        throw new Error(error?.message || "Failed to create seller");
-      }
-      return await res.json();
-    },
-    onSuccess: () => {
-      toast.success("Seller Created Successfully");
-      queryClient.invalidateQueries({ queryKey: ["sellers"] });
-    },
-    onError: (err: Error) => {
-      toast.error(err?.message || "Error creating Seller");
-    },
-  });
-};
