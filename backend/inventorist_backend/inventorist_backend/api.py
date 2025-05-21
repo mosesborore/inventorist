b10f3b956d82f1f9e81edd95da6e9276c0e39039
Moses Borore <mosesbororekinuthia@gmail.com>
2025-05-21 10:09:29 +0300

    Add health check route

--- backend/inventorist_backend/inventorist_backend/api.py
+++ backend/inventorist_backend/inventorist_backend/api.py
@@ -6,5 +6,10 @@ from products.api import router as product_router
 api = NinjaAPI(title="Inventorist API", description="Inventory Tracker")
 
 
+@api.get("/health")
+def health_check(request):
+    return {"status": "ok"}
+
+
 api.add_router("/products/", product_router)
 api.add_router("/invoices/", invoice_router)
